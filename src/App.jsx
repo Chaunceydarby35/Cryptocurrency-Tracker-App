0422ddc1873e9453d70c3e84783d1a7296d3159f
Chauncey Darby <chaunceydrb@gmail.com>
2024-05-05 23:43:38 +0200

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,22 @@ function App() {
           {currency.filter((val)=> {
             return val.name.toLowerCase().includes(search.toLowerCase())
           }).map((val) => {
-            return tr
+            return <tr>
+              <td className="rank">{val.rank}</td>
+              <td className="logo">
+                <a href={val.websiteUrl}>
+                  <img src={val.icon} alt=""/>
+                </a>
+                <p>{val.name}</p>
+              </td>
+              <td className="symbol">
+                {val.symbol}
+              </td>
+              <td>${val.marketCap}</td>
+              <td>${val.price.toFixed(2)}</td>
+              <td>{val.availableSupply}</td>
+              <td>{val.volume.toFixed(0)}</td>
+            </tr>
           })}
         </tbody>
       </table>
